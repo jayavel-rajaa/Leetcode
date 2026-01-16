449593e400528c8c17abaefffec55310c4516191
Jayavel Rajaa VR <jayavelrajaavr@gmail.com>
2026-01-16 15:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (93.68%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,24 +1,11 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        if(n==0)
-            return 1;
-        if(n==1)
-            return 10;
-        if(n==2)
-            return 91;
-        if(n==3)
-            return 91+(9*9*8);
-        if(n==4)
-            return 91+(9*9*8)+(9*9*8*7);
-        if(n==5)
-            return 91+(9*9*8)+(9*9*8*7)+(9*9*8*7*6);  
-        if(n==6)
-            return 91+(9*9*8)+(9*9*8*7)+(9*9*8*7*6)+(9*9*8*7*6*5);      
-        if(n==7)
-            return 91+(9*9*8)+(9*9*8*7)+(9*9*8*7*6)+(9*9*8*7*6*5)+(9*9*8*7*6*5*4);
-        if(n==8)
-            return 91+(9*9*8)+(9*9*8*7)+(9*9*8*7*6)+(9*9*8*7*6*5)+(9*9*8*7*6*5*4)+(9*9*8*7*6*5*4*3);    
-        else    
-            return 0;  
-    }   
+        if (n == 0) return 1;
+        if (n == 1) return 10;
+        int k = 9;
+        for (int i = 0; i < n - 1; i++) {
+            k *= (9 - i);
+        }
+        return k + countNumbersWithUniqueDigits(n - 1);
+    }
 }
