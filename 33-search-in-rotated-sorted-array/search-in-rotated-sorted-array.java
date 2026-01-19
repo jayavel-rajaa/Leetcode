b0c8bc6adbdc456559d7418965ae1b110e39fe76
Jayavel Rajaa VR <jayavelrajaavr@gmail.com>
2026-01-19 23:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (33.43%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int r=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == target){
+                r = i;
+                break;
+            }                
+        }
+        return r;
+    }
+}
